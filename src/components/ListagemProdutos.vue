a8830b87f96365cee61a970a8c4d1a8e05844517
iTzMvz <matheus.vidal.zenere@gmail.com>
2024-02-21 15:16:23 -0300

    FEAT LOJA: Implementando itens na loja

---
+++ src/components/ListagemProdutos.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
+
+const produtos = ref([]);
+
+onMounted(async () => {
+  const response = await axios.get('https://fakestoreapi.com/products');
+  produtos.value = response.data;
+});
+</script>
+
+<template>
+  <div>
+    <h1>Produtos</h1>
+    <div v-for="produto in produtos" :key="produto.id">
+      <h2>{{ produto.title }}</h2>
+      <p>{{ produto.description }}</p>
+      <p>{{ produto.price }}</p>
+      <img :src="produto.image" :alt="produto.title" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+img {
+  max-width: 100%;
+  height: auto;
+}
+</style>
